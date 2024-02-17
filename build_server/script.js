076e32fffe02a901594381209c0f03b3ad58ee5a
IMRANDIL <aliimranadil2@gmail.com>
2024-02-17 06:50:24 +0530

    script.js modified

--- build_server/script.js
+++ build_server/script.js
@@ -1,24 +1,44 @@
-const {exec} = require('child_process')
+const { exec } = require('child_process');
 const path = require('path');
+const fs = require('fs');
 
-(initTheAction = async ()=>{
+(async () => {
+    console.log('Executing script.js');
+    
+    const outDirPath = path.join(__dirname, 'output');
+    
+    try {
+        await new Promise((resolve, reject) => {
+            const process = exec(`cd ${outDirPath} && npm install && npm run build`);
+            
+            process.stdout.on('data', (data) => {
+                console.log(data.toString());
+            });
+            
+            process.stderr.on('data', (data) => {
+                console.error(data.toString());
+            });
+            
+            process.on('close', (code) => {
+                if (code === 0) {
+                    console.log('Code build done!');
+                    resolve();
+                } else {
+                    reject(new Error(`Build process exited with code ${code}`));
+                }
+            });
+        });
+        
+        const distFolderPath = path.join(__dirname, 'output', 'dist');
+        const distFolderContents = fs.readdirSync(distFolderPath, { recursive: true });
 
-console.log('Executing script.js')
-
-const outDirPath = path.join(__dirname, 'output')
-const process = exec(`cd ${outDirPath} && npm install && npm run build`)
-
-process.stdout.on('data', (data)=>{
-    console.log(data.toString())
-});
-
-
-process.stdout.on('error', (data)=>{
-    console.log('Error ->', data.toString())
-});
-
-process.on('close', ()=>{
-    console.log('Code build done!')
-})
-
-})()
+        for(const filePath of distFolderContents) {
+            if(fs.lstatSync(filePath).isDirectory) {
+                continue;
+            }
+            
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+})();
