5c1a1de9bb418d7f1ac5f86b25e4b2e21c14623c
IMRANDIL <aliimranadil2@gmail.com>
2024-02-17 12:02:48 +0530

    some more proxy

--- s3-reverse-proxy/index.js
+++ s3-reverse-proxy/index.js
@@ -16,6 +16,15 @@ app.use((req, res)=>{
     const resolvesTo = `${baseURI}/${subDomain}`;
 
     return proxy.web(req,res, {target: resolvesTo, changeOrigin: true})
+});
+
+proxy.on('proxyReq', (proxyReq, req, res)=>{
+    const url = req.url;
+
+    if(url == '/') {
+        proxyReq.path += 'index.html';
+      
+    }
 })
 
 
