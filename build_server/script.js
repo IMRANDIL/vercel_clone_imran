8723d62ba40936ed654479020380eb6ca3e989d7
IMRANDIL <aliimranadil2@gmail.com>
2024-02-17 11:22:40 +0530

    some more logs

--- build_server/script.js
+++ build_server/script.js
@@ -1,72 +1,71 @@
-const { exec } = require('child_process');
-const path = require('path');
-const fs = require('fs');
-const mime = require('mime-types')
-const {S3Client, PutObjectCommand} = require('@aws-sdk/client-s3')
-
+const { exec } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const mime = require("mime-types");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
 const s3Client = new S3Client({
-    region: process.env.region,
-    credentials: {
-        accessKeyId: process.env.accessKeyId,
-        secretAccessKey: process.env.secretAccessKey
-    }
-})
+  region: process.env.region,
+  credentials: {
+    accessKeyId: process.env.accessKeyId,
+    secretAccessKey: process.env.secretAccessKey,
+  },
+});
 
 const PROJECT_ID = process.env.PROJECT_ID;
 
-
 (async () => {
-    console.log('Executing script.js');
-    
-    const outDirPath = path.join(__dirname, 'output');
-    
+    console.log("Executing script.js");
+  
+    const outDirPath = path.join(__dirname, "output");
+  
     try {
-        await new Promise((resolve, reject) => {
-            const process = exec(`cd ${outDirPath} && npm install && npm run build`);
-            
-            process.stdout.on('data', (data) => {
-                console.log(data.toString());
-            });
-            
-            process.stderr.on('data', (data) => {
-                console.error(data.toString());
-            });
-            
-            process.on('close', (code) => {
-                if (code === 0) {
-                    console.log('Code build done!');
-                    resolve();
-                } else {
-                    reject(new Error(`Build process exited with code ${code}`));
-                }
-            });
+      await new Promise((resolve, reject) => {
+        const process = exec(`cd ${outDirPath} && npm install && npm run build`);
+  
+        process.stdout.on("data", (data) => {
+          console.log(data.toString());
         });
-        
-        const distFolderPath = path.join(__dirname, 'output', 'dist');
-        const distFolderContents = fs.readdirSync(distFolderPath, { recursive: true });
-
-        for(const filePath of distFolderContents) {
-            if(fs.lstatSync(filePath).isDirectory) {
+  
+        process.stderr.on("data", (data) => {
+          console.error(data.toString());
+        });
+  
+        process.on("close", async (code) => {
+          
+            const distFolderPath = path.join(__dirname, "output", "dist");
+            const distFolderContents = fs.readdirSync(distFolderPath, {
+              recursive: true,
+            });
+  
+            for (const file of distFolderContents) {
+              const filePath = path.join(distFolderPath, file);
+              if (fs.lstatSync(filePath).isDirectory()) {
                 continue;
-            }
-            console.log('Uploading', filePath)
-            const putCommandForS3 = new PutObjectCommand({
-                Bucket: 'vercel-clone-imran',
+              }
+              console.log("Uploading", filePath);
+              const putCommandForS3 = new PutObjectCommand({
+                Bucket: "vercel-clone-imran",
                 Key: `__outputs/${PROJECT_ID}/${filePath}`,
                 Body: fs.createReadStream(filePath),
-                ContentType:mime.lookup(filePath)
-            });
-
-            try {
+                ContentType: mime.lookup(filePath),
+              });
+  
+              try {
                 await s3Client.send(putCommandForS3);
                 console.log(`Uploaded ${filePath} to S3`);
-            } catch (error) {
+              } catch (error) {
                 console.error(`Error uploading ${filePath} to S3:`, error);
+                reject(error);
+              }
             }
-        }
-        console.log('Done')
+            console.log('Done...');
+            resolve()
+        });
+
+      });
     } catch (error) {
-        console.error('Error:', error.message);
+      console.error("Error:", error.message);
     }
-})();
+  })();
+  
