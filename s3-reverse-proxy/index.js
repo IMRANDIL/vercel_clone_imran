372ae3274fefcae312cfe6f8dec89653faa26389
IMRANDIL <aliimranadil2@gmail.com>
2024-02-17 11:43:59 +0530

    proxy added

--- s3-reverse-proxy/index.js
+++ s3-reverse-proxy/index.js
@@ -1,1 +1,25 @@
-const express = require('express')
+const express = require('express');
+const httpProxy = require('http-proxy')
+const app = express();
+const path = require('path')
+
+
+const PORT = process.env.PORT || 8000
+
+const baseURI = `https://vercel-clone-imran.s3.amazonaws.com/__outputs`
+const proxy = httpProxy.createProxy()
+
+app.use((req, res)=>{
+    const hostName = req.hostname;
+    const subDomain = hostName.split('.')[0];
+
+    const resolvesTo = `${baseURI}/${subDomain}`;
+
+    return proxy.web(req,res, {target: resolvesTo, changeOrigin: true})
+})
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Proxy running on port:${PORT}`)
+})
